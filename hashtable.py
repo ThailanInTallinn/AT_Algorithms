5ec44ea52a66758a9904addd094c83ebc7be6a6e
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-03-28 20:30:42 +0200

    modifies a few method names

--- hashtable.py
+++ hashtable.py
@@ -1,4 +1,4 @@
-class Hashtable:
+class HashTableChained:
     def __init__(self, tamanho):
         self.tamanho = tamanho
         self.tabela = [[] for _ in range(tamanho)]
@@ -6,36 +6,49 @@ class Hashtable:
     def _funcao_hash(self, chave):
         return chave % self.tamanho
 
-    def inserir(self, chave, valor):
+    def put(self, chave, valor):
         index = self._funcao_hash(chave)
         bucket = self.tabela[index]
+        counter = 0
 
         for i, (chave_ex, _) in enumerate(bucket):
+            counter += 1
             if(chave_ex == chave):
                 bucket[i] = (chave, valor)
                 return 
-            
+        print(f"Número de comparações para inserção: {counter}") 
         bucket.append((chave, valor))
 
 
 
-    def buscar(self, chave):
+    def get(self, chave):
+        counter = 0
         index = self._funcao_hash(chave)
         bucket = self.tabela[index]
 
         for chave_ex, valor in bucket:
+            counter += 1
             if(chave_ex == chave):
                 return valor
-
+        print(f"Número de comparações para busca: {counter}")
         return None
 
-    def remover(self, chave):
+    def delete(self, chave):
         index = self._funcao_hash(chave)
         bucket = self.tabela[index]
+        counter = 0
 
         for i, (chave_ex, valor) in enumerate(bucket):
+            counter += 1
             if chave_ex == chave:
                 del bucket[i]
 
+        print(f"Número de comparações para deleção: {counter}")
         return None
 
+    def __len__(self):
+        count = 0
+        for item in self.tabela:
+            count += len(item)
+
+        return count
