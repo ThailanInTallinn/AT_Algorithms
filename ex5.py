bf689ca909e43365b2053886f73493d6f94a41e1
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-03-31 15:08:32 +0300

    traverse tree and stores nodes in a stack

--- ex5.py
+++ ex5.py
@@ -8,12 +8,23 @@ lista_original = gerar_lista(100, aleatorio=True)
 for i in lista_original:
     tree.inserir(i)
 
+
 # Fila como estrutura de dados auxiliar
+
 fila = Fila(150)
 fila.preencher()
 for item in tree.traverse_em_ordem(tree.raiz):
     fila.adicionar(item)
 
+print("=" * 20, "FILA", "="*20) 
 fila.exibir()
 
 # Pilha como estrutura de dados auxiliar
+
+pilha = Pilha(150)
+
+for item in tree.traverse_em_ordem(tree.raiz):
+    pilha.adicionar(item)
+print("=" * 20, "PILHA", "="*20) 
+pilha.exibir()
+
